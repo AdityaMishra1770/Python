f4c3ce8dde65852b10cf4afd80948fd193db666c
AdityaMishra1770 <142557570+AdityaMishra1770@users.noreply.github.com>
2023-09-10 07:33:47 -0700

    Update Word_merger.py

--- Word_merger.py
+++ Word_merger.py
@@ -1,27 +1,29 @@
-def onewordextactor(word,n):
-    c=0
+def one_word_extractor(word, n):
+    c: int = 0
     for i in word:
-        c+=1
-        if c==n :
+        c += 1
+        if c == n:
             return i
-def word_Merger(word1,word2):
-    output=""
-    if len(word1)>len(word2):
-        s=len(word2)
-        g=len(word1)
-        yo=1
-    else :
-        s=len(word1)
-        g=len(word2)
-        yo=2
+
+
+def word_merger(word1, word2):
+    output = ""
+    if len(word1) > len(word2):
+        s = len(word2)
+        g = len(word1)
+        yo = 1
+    else:
+        s = len(word1)
+        g = len(word2)
+        yo = 2
 
     for i in range(s):
-        output=output+onewordextactor(word1,i+1)+onewordextactor(word2,i+1)
-    o=""
-    if yo==1:
-        for j in range(s,g):
-            o=o+onewordextactor(word1,j+1)
+        output = output + one_word_extractor(word1, i + 1) + one_word_extractor(word2, i + 1)
+    o = ""
+    if yo == 1:
+        for j in range(s, g):
+            o = o + one_word_extractor(word1, j + 1)
     else:
-        for jo in range(s,g):
-            o=o+onewordextactor(word2,jo+1)
-    return output+o
+        for jo in range(s, g):
+            o = o + one_word_extractor(word2, jo + 1)
+    return output + o
