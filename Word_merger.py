89df639c4733150c815d7bbe85efc04acc0ad1ef
Aditya Mishra <am14156789@gmail.com>
2023-10-21 01:31:40 +0530

    Updated

--- Word_merger.py
+++ Word_merger.py
@@ -1,29 +1,6 @@
-def one_word_extractor(word, n):
-    c: int = 0
-    for i in word:
-        c += 1
-        if c == n:
-            return i
-
-
-def word_merger(word1, word2):
-    output = ""
-    if len(word1) > len(word2):
-        s = len(word2)
-        g = len(word1)
-        yo = 1
-    else:
-        s = len(word1)
-        g = len(word2)
-        yo = 2
-
-    for i in range(s):
-        output = output + one_word_extractor(word1, i + 1) + one_word_extractor(word2, i + 1)
-    o = ""
-    if yo == 1:
-        for j in range(s, g):
-            o = o + one_word_extractor(word1, j + 1)
-    else:
-        for jo in range(s, g):
-            o = o + one_word_extractor(word2, jo + 1)
-    return output + o
+def word_merger(a,b):
+    added = ""
+    for i in range(min(len(a),len(b))):
+        added += a[i]+b[i]
+    return added
+print(word_merger("hello", "world"))
